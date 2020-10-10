3185737e5be8c3d2016acbdc3150f070856c46de
Ron <rumbletumjum@gmail.com>
2020-10-10 05:46:01 -0400

    [config.def.h] shrink gaps, move bar back

--- config.def.h
+++ config.def.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "misc Tamsyn:size=8" };
-static const char dmenufont[]       = "misc Tamsyn:size=8";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "misc Termsyn:size=8" };
+static const char dmenufont[]       = "misc Termsyn:size=8";
 
 static const char fg[]		    = "#b9b9b9";
 
