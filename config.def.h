d58baa350acec4db2a2b556934e9bb08828633f7
Ron <rumbletumjum@gmail.com>
2020-10-12 05:47:53 -0400

    update font

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 4;       /* vert outer gap between windows
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "misc Termsyn:size=8" };
-static const char dmenufont[]       = "misc Termsyn:size=8";
+static const char *fonts[]          = { "PragmataPro:size=9" };
+static const char dmenufont[]       = "PragmataPro:size=9";
 
 static const char fg[]		    = "#b9b9b9";
 
