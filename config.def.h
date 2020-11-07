959049b0ccf9e9fa0656ac10c83bfe8c429bd392
Ron <rumbletumjum@gmail.com>
2020-11-07 13:01:50 -0500

    font size, bar position, border width

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 #define TERMINAL "st"
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -12,10 +12,10 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 
-static const char *fonts[]               = { "monospace:size=9" };
-static const char dmenufont[]            = "monospace:size=9";
+static const char *fonts[]               = { "xos4 Terminus:pixelsize=28:style=bold:antialias=false:autohint=false" };
+static const char dmenufont[]            = "xos4 Terminus:pixelsize=28:style=bold:antialias=false:autohint=false";
 
 static const char norm_fg[]              = "#333333";
 static const char norm_bg[]              = "#ffffff";
