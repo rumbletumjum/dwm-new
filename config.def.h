669c145639a20229129e4b906875a1309869a1bc
Ron <rumbletumjum@gmail.com>
2020-11-07 23:05:59 -0500

    LoDPI

--- config.def.h
+++ config.def.h
@@ -10,13 +10,13 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 
-static const char *fonts[]               = { "xos4 Terminus:pixelsize=28:style=bold:antialias=false:autohint=false" };
-static const char dmenufont[]            = "xos4 Terminus:pixelsize=28:style=bold:antialias=false:autohint=false";
+static const char *fonts[]               = { "xos4 Terminus:pixelsize=10:style=bold:antialias=false:autohint=false" };
+static const char dmenufont[]            = "xos4 Terminus:pixelsize=10:style=bold:antialias=false:autohint=false";
 
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
