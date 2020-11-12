3617df3702f3facd8057334384dab6ac62fb8839
Ron <rumbletumjum@gmail.com>
2020-11-12 17:22:09 -0500

    let dmenu & xrdb handle dmenu font + remove windowmenu

--- config.def.h
+++ config.def.h
@@ -180,9 +180,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static       char dmenumon[2]      = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_bblue, "-sf", col_black, "-h", "30", NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-nb", normbg, "-nf", normfg, "-sb", selbg, "-sf", selfg, "-h", "30", NULL };
 static const char *termcmd[]       = { "st", "-e", "fish", NULL };
-static const char *windowmenucmd[] = { "windowmenu", NULL };
 static const char *browsercmd[]    = { BROWSER, NULL };
 
 
